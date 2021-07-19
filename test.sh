c2c3473ada2fb7ecae95ab923742540be3178d22
ChromaCat248 <chromacat248@gmail.com>
2021-07-19 00:27:14 -0400

    I fixed the xkbcomp flooding so I don't need the "Warning: Could not resolve keysym" messages anymore but I just commented it rather than deleting it just in case someone who wants to test it has similar issues

--- test.sh
+++ test.sh
@@ -6,10 +6,10 @@ echo compiling...
 make
 echo done compiling.
 
-# Band-aid solution to the problem of "could not resolve keysym" flooding, once I figure out why that happens this won't be needed
-echo
-read -p "Press Ctrl+C to abort or press any other key to continue." -n1 -s
-echo
+# Uncomment if you get xkbcomp flooding and need time to look at compiler errors
+#echo
+#read -p "Press Ctrl+C to abort or press any other key to continue." -n1 -s
+#echo
 
 XEPHYR=$(whereis -b Xephyr | cut -f2 -d' ')
 xinit ./xinitrc -- \
