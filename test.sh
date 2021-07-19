4db11eb9105051a372b8435ba2d6339da2b03a98
ChromaCat248 <chromacat248@gmail.com>
2021-07-18 23:40:09 -0400

    Added a "press any key to continue" message as a band-aid solution to "Warning: Could not resolve keysym" flooding on my end.

--- test.sh
+++ test.sh
@@ -2,10 +2,14 @@
 
 # To run a command inside the window manager, open a terminal and set the DISPLAY environment variable to ":100"
 
-echo
 echo compiling...
 make
 echo done compiling.
+
+# Band-aid solution to the problem of "could not resolve keysym" flooding, once I figure out why that happens this won't be needed
+echo
+read -p "Press Ctrl+C to abort or press any other key to continue." -n1 -s
+echo
 
 XEPHYR=$(whereis -b Xephyr | cut -f2 -d' ')
 xinit ./xinitrc -- \
