6a67cf610c6211dc4474d123f244b7aff4084cb3
ChromaCat248 <50424765+ChromaCat248@users.noreply.github.com>
2021-08-24 00:14:13 -0400

    Build directory shouldn't be in the repository

--- target/debug/build/x11-5eb8c2f13ff3d539/build_script_build-5eb8c2f13ff3d539.d
+++
@@ -1,5 +0,0 @@
-/home/chromacat248/Documents/source-code/zwin/target/debug/build/x11-5eb8c2f13ff3d539/build_script_build-5eb8c2f13ff3d539: /home/chromacat248/.cargo/registry/src/github.com-1ecc6299db9ec823/x11-2.18.2/build.rs
-
-/home/chromacat248/Documents/source-code/zwin/target/debug/build/x11-5eb8c2f13ff3d539/build_script_build-5eb8c2f13ff3d539.d: /home/chromacat248/.cargo/registry/src/github.com-1ecc6299db9ec823/x11-2.18.2/build.rs
-
-/home/chromacat248/.cargo/registry/src/github.com-1ecc6299db9ec823/x11-2.18.2/build.rs:
