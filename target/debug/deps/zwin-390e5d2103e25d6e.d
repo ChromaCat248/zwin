15238b8685e689058ebd74fcae7a127e9d7610c1
ChromaCat248 <chromacat248@gmail.com>
2021-08-23 22:19:30 -0400

    Add KDevelop project file to .gitignore

---
+++ target/debug/deps/zwin-390e5d2103e25d6e.d
@@ -0,0 +1,7 @@
+/home/chromacat248/Documents/source-code/zwin/target/debug/deps/zwin-390e5d2103e25d6e: src/main.rs src/../config.rs src/event_handling.rs
+
+/home/chromacat248/Documents/source-code/zwin/target/debug/deps/zwin-390e5d2103e25d6e.d: src/main.rs src/../config.rs src/event_handling.rs
+
+src/main.rs:
+src/../config.rs:
+src/event_handling.rs:
---
+++ target/debug/zwin.d
@@ -0,0 +1,1 @@
+/home/chromacat248/Documents/source-code/zwin/target/debug/zwin: /home/chromacat248/Documents/source-code/zwin/config.rs /home/chromacat248/Documents/source-code/zwin/src/event_handling.rs /home/chromacat248/Documents/source-code/zwin/src/main.rs
